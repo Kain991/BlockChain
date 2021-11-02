5b9be897691be33ddafd5a8dee608ec21673b381
Kain991 <74097913+Kain991@users.noreply.github.com>
2021-11-02 23:25:22 +0600

    Add files via upload

--- BlockChain/main.cpp
+++ BlockChain/main.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <thread>
+#include <chrono>
+#include <ctime>
+#include <mutex>
 using namespace std;
 struct TransactionData
 {
@@ -175,32 +179,123 @@ void Blockchain::printBlockchain() {
             <<endl<<"Is Block Valid?: "<<currentBlock.isHashValid();
     }
 }
+mutex Mutex;
+class Miner{
+
+
+    bool checkCaptcha(string &captcha, string &user_captcha)
+    {
+        return captcha.compare(user_captcha) == 0;
+    }
+    string generateCaptcha(int n)
+    {
+        time_t t;
+        srand((unsigned)time(&t));
+        // Characters to be included
+        char *chrs = "abcdefghijklmnopqrstuvwxyzABCDEFGHI"
+                     "JKLMNOPQRSTUVWXYZ0123456789";
+        // Generate n characters from above set and
+        // add these characters to captcha.
+        string captcha = "";
+        while (n--)
+            captcha.push_back(chrs[rand()%62]);
+        return captcha;
+    }
+    void Captcha(){
+        string s = generateCaptcha(56);
+        cout<<"input the captcha:";
+        string s1;
+        cin>>s1;
+       if(checkCaptcha(s , s1 )){
+           cout<<"Incorrect";
+       }
+       else {
+           cout<<"Correct! Good Luck!";
+       }
+    }
+    void mine(TransactionData data , Blockchain blockchain){
+        srand(time(0));
+        for(int j = 0; j < 100000;j++)
+        for(int i = 0; i < 100000;i++)
+            for(int k =0; k < 10000;k++)
+                for(int h = 0; h < 100000;h++);
+
+
+    }
+};
+
+
+double getTotalVolume(Blockchain& chain) {
+    double total = 0;
+    for (auto iter = chain.begin(); iter != chain.end(); ++iter) {
+        total = total + iter->getTransaction_data().amount;
+    }
+    return total;
+}
+
+void shared_mine(Miner& miner, TransactionData& tt, Blockchain& blockchain) {
+    lock_guard<mutex> guard(Mutex);
+Miner miner1;
+    miner1.mine(tt, blockchain);
+}
+
+void func(Miner& miner, TransactionData& tt, Blockchain& blockchain) {
+    for (int i = 0; i < 3; ++i) {
+        shared_mine(miner, tt, blockchain);
+    }
+}
+
 int main()
 {
+    Blockchain blockchain;
+    future<double> ref = async(launch::deferred, getTotalVolume, ref(blockchain));
+    time_t current;
+    TransactionData act( 9, "Kainar", "Dastan", time(&current));
+    TransactionData act1(666, "Edilbek", "Yernur", time(&current));
+    Miner miner1;
+    Miner miner2;
 
+    blockchain.printBlockchain();
+    thread thread1(func, ref(miner1), ref(act), ref(blockchain));
+    thread thread2(func, ref(miner2), ref(act1), ref(blockchain));
+
+    thread1.join();
+    thread2.join();
+    blockchain.printBlockchain();
+
+
+    if (blockchain.isBlockchainValid()) {
+        cout << "Work" << endl;
+    }
+    cout << ref.get() << endl;
+    cout << getTotalVolume(blockchain) << endl;
     Blockchain awesomeCoin;
-
     time_t data1Time;
-    TransactionData data1(1.5, "Joe", "Sally", time_t(&data1Time));
+    TransactionData data1(1.5, "Kainar", "Edilbek", time_t(&data1Time));
     awesomeCoin.addBlock(data1);
-
     time_t data2Time;
-    TransactionData data2(0.2233, "Martha", "Fred", time_t(&data2Time));
+    TransactionData data2(0.2233, "Dastan", "Nurzhas", time_t(&data2Time));
     awesomeCoin.addBlock(data2);
-
     awesomeCoin.printBlockchain();
-
-
-    printf("\nIs chain still valid? %d\n", awesomeCoin.isChainValid());
-
+    cout<<endl<<"Is chain valid?\n";
+    if(awesomeCoin.isChainValid()){
+        cout<<"Valid";
+    }
+    else {
+        cout<<"Invalid";
+    }
     Block *hackBlock = awesomeCoin.getLatestBlock();
     hackBlock->data.amount = 10000;
-    hackBlock->data.receiverKey = "Jon";
-
-
+    hackBlock->data.receiverKey = "Kainar";
     awesomeCoin.printBlockchain();
+    cout<<endl<<"Is chain valid?\n";
+    if(awesomeCoin.isChainValid()){
+        cout<<"Valid";
+    }
+    else {
+        cout<<"Invalid";
+    }
 
-    printf("\nIs chain still valid? %d\n", awesomeCoin.isChainValid());
 
     return 0;
 }
